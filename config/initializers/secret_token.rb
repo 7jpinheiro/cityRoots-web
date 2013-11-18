c19c9559204ffb099dbb0260af0c7c5dac653876
catarinasoliveira <catarina.sofia2@gmail.com>
2013-11-18 22:52:56 +0000

    First Commit with code generated by rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CityRootsWeb::Application.config.secret_key_base = 'c1fc6a5c4cad5795ccc95976752ff798091d73284cd1f003e1b04bf5a28e25a5d8ba10a636ec360b94e711e1ff5fc652a6050103ce4847e596387b52ae146025'
