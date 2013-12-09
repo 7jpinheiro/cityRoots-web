8da309e010f463570a212f625180ef1038517e26
JProphet89 <jprophet89@gmail.com>
2013-12-09 17:33:49 +0000

    index e pacotes

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,6 @@
 class WelcomeController < ApplicationController
   def index
   end
+  def pacotes
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 CityRootsWeb::Application.routes.draw do
   
   resources :countries
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   root to:'welcome#index'
 
+
   # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+  get 'welcome/:pacotes' => 'welcome#pacotes'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
