b816664eef6983d488fa802421539963734b19ec
catarinasoliveira <catarina.sofia2@gmail.com>
2013-12-17 14:21:31 +0000

    Users tentativa

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  before_filter :configure_permitted_parameters, :only => [:create]
 
   protect_from_forgery with: :exception
+
+  protected
+  def configure_permitted_parameters
+      devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:city_id, :email, :password) }
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
 	validates :email, :encrypted_password, :city_id, :language_id, presence: true
 	belongs_to :city
 	belongs_to :language
+	
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
