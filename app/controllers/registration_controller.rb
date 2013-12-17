5b27278486967f233f4d8acacc2f23e3dda64183
Rui Brito <ruibrito666@gmail.com>
2013-12-17 16:18:23 +0000

    devise working

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -1,27 +1,30 @@
 class RegistrationController < ApplicationController
 
     def new
-        @user = User.new;
+        @user = User.new
+        @city = City.new
     end
 
-    def create 
+    def create
 
         @user = user.new
         # @user.username = params[:member][:username]
-        @user.email = params[:member][:email]
-        @user.password = params[:member][:password]
-        @user.password_confirmation =params[:member][:password_confirmation]
+        @user.email = params[:user][:email]
+        @user.password = params[:user][:password]
+        @user.password_confirmation =params[:user][:password_confirmation]
+
+
+        @city = City.new
+        @city.city_id = params[:city][:id]
 
 
         @user.valid?
 
         if @user.errors.blank?
             @user.save
+            @city.save
         else
             render :action => "new"
         end
-
-
     end
-
 end
