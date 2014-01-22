57b90e5c927b0a0be3140414cf0a18ec509d808b
Jose Alves <zealves.080@gmail.com>
2014-01-22 03:25:23 +0000

    pt3

--- app/controllers/mobile_users_controller.rb
+++ app/controllers/mobile_users_controller.rb
@@ -29,7 +29,7 @@ class MobileUsersController < ApplicationController
 
     respond_to do |format|
       if @mobile_user.save
-        format.html { redirect_to @mobile_user, notice: 'Mobile user was successfully created.' }
+        format.html { redirect_to @mobile_user, notice: 'Utilizador mobile criado com sucesso.' }
         format.json { render action: 'show', status: :created, location: @mobile_user }
       else
         format.html { render action: 'new' }
@@ -43,7 +43,7 @@ class MobileUsersController < ApplicationController
   def update
     respond_to do |format|
       if @mobile_user.update(mobile_user_params)
-        format.html { redirect_to @mobile_user, notice: 'Mobile user was successfully updated.' }
+        format.html { redirect_to @mobile_user, notice: 'Utilizador mobile actualizado com sucesso.' }
         format.json { head :no_content }
       else
         format.html { render action: 'edit' }
