ace1b19eb5086008d1b28243bbb61206569d8e87
7jpinheiro <7jpinheiro@gmail.com>
2014-01-28 17:51:35 +0000

    Mensagem de erro de login falhado pelo fb

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -8,6 +8,9 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     if @user.persisted?
       sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
       set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
+    elsif @user == false
+      flash[:notice] = "Já esta registado com um email igual ao que usa no facebook! Use esse email para fazer login!"
+      redirect_to new_user_registration_url
     else
       session["devise.facebook_data"] = request.env["omniauth.auth"].except("extra")
       redirect_to new_user_registration_url
